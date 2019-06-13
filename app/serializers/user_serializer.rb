e80a753659e3bfd9e7dc92e75a21d8d8985413ca
ferryrules <ferris@ferryrules.com>
2019-06-13 13:31:35 -0400

    new serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,22 +1,20 @@
 class UserSerializer < ActiveModel::Serializer
-  has_many :user_folders
+  # has_many :user_folders
   has_many :folders, through: :user_folders
-  has_many :notes, through: :folders
+  # has_many :notes, through: :folders
 
-  attributes :id, :name, :username, :folders, :notes
+  attributes :id, :name, :username, :folders
 
   def folders
     self.object.folders.map do |fold|
       {name: fold.name,
-       id: fold.id}
+       id: fold.id,
+       notes: fold.notes.map do |n|
+         {note: n.note,
+         id: n.id,
+         url: n.url}
+       end}
     end
   end
 
-  def notes
-    self.object.notes.map do |note|
-      {note: note.note,
-       id: note.id,
-       url: note.url}
-    end
-  end
 end
