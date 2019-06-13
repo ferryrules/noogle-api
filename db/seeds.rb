0d5472e9b873328c621c0f383c967be667a51e2a
ferryrules <ferris@ferryrules.com>
2019-06-13 09:34:31 -0400

    second folder test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 User.create(name: "Ferris Boran", username: "ferryrules", password: "dnj547", img:"~/Desktop/camel_rangler.jpg")
 
 Folder.create(name: "Ruby Notes")
+Folder.create(name: "Why Danielle is Hot")
 
 Note.create(note: "Ruby is awesome!!", url: "https://www.iloveruby.com", folder_id: 1)
 
