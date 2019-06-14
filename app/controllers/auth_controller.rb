a5a40dd421fdb7f6900265b5d657d90432e05606
ferryrules <ferris@ferryrules.com>
2019-06-14 13:55:50 -0400

    login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,18 +1,18 @@
 class AuthController < ApplicationController
-
   def login
-    #find the user by their username
-    user = User.find_by(username: params['username'])
+    # params: { username: '', password: '' }
 
-    #authenticate the user
-    is_authenticated = user.authenticate(params['password'])
+    # find the user by their username
+    user = User.find_by(username: params["username"])
 
-    #'log in the user'
+    # authenticate the user
+    is_authenticated = user.authenticate(params["password"])
+
+    # "log in the user" - whatever that means
     if is_authenticated
       render json: { token: encode_token(user) }
     else
       render json: { error: "Wrong username or password" }
     end
   end
-
 end
