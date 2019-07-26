c29a4b915340ede06b8a7e289539d9c7cbb26175
ferryrules <ferris@ferryrules.com>
2019-07-26 12:45:47 -0500

    [ADD] url and username to note serializer

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -2,6 +2,10 @@ class NoteSerializer < ActiveModel::Serializer
   belongs_to :folder
   belongs_to :user
 
-  attributes :id, :note, :user, :folder, :created_at, :updated_at
+  attributes :id, :note, :url, :username, :user, :folder, :created_at, :updated_at
 
+  def username
+    self.object.user.username
+  end
+  
 end
